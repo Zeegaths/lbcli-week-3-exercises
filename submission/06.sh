51d285a0d812e65ae92e7350ad6a1294a5865dbe
Zeegaths <your.email@example.com>
2025-04-03 13:28:07 +0300

    submission

--- submission/06.sh
+++ submission/06.sh
@@ -1,2 +1,12 @@
 # What is the hash of this partially signed transaction?
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+#!/bin/bash
+
+# The PSBT to decode
+PSBT="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
+
+# Decode the PSBT and extract the hash
+HASH=$(bitcoin-cli -named decodepsbt psbt=$PSBT | jq -r '.tx.hash')
+
+# Output the hash
+echo $HASH
--- submission/07.sh
+++ submission/07.sh
@@ -1,2 +1,12 @@
 # What is the receiver's address in this partially signed transaction?
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+#!/bin/bash
+
+# The PSBT to decode
+PSBT="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
+
+# Decode the PSBT and extract the receiving address from vout[0]
+ADDRESS=$(bitcoin-cli -named decodepsbt psbt=$PSBT | jq -r '.tx.vout[0].scriptPubKey.addresses[0]')
+
+# Output the address
+echo $ADDRESS
