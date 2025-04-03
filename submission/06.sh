702ad9ad824432c977e6bd8790252bc2177f2c3d
Zeegaths <your.email@example.com>
2025-04-03 15:25:44 +0300

    submission

--- submission/06.sh
+++ submission/06.sh
@@ -6,7 +6,7 @@
 PSBT="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
 
 # Decode the PSBT and extract the hash
-HASH=$(bitcoin-cli -named decodepsbt psbt=$PSBT | jq -r '.tx.hash')
+HASH=$(bitcoin-cli -regtest decodepsbt psbt=$PSBT | jq -r '.tx.hash')
 
 # Output the hash
 echo $HASH
