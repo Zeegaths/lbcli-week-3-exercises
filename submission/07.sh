8dd2748b4411d91b57c50a0381f86b6d613c86fc
Zeegaths <your.email@example.com>
2025-04-03 15:32:14 +0300

    submission

--- submission/07.sh
+++ submission/07.sh
@@ -6,7 +6,7 @@
 PSBT="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
 
 # Decode the PSBT and extract the receiving address from vout[0]
-ADDRESS=$(bitcoin-cli -named decodepsbt psbt=$PSBT | jq -r '.tx.vout[0].scriptPubKey.addresses[0]')
+ADDRESS=$(bitcoin-cli -regtest -named decodepsbt psbt=$PSBT | jq -r '.tx.vout[0].scriptPubKey.addresses[0]')
 
 # Output the address
 echo $ADDRESS
