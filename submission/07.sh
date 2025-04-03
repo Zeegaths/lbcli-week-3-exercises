13740a618af548142419759e4e2c40e8eb2bbb01
Zeegaths <your.email@example.com>
2025-04-03 15:42:11 +0300

    submission

--- submission/07.sh
+++ submission/07.sh
@@ -2,11 +2,24 @@
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
 #!/bin/bash
 
-# The PSBT to decode
-PSBT="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
+# Input transaction has two inputs from same txid but different vouts
+TXID="23c19f37d4e92e9a115aab86e4edc1b92a51add4e0ed0034bb166314dde50e16"
+VOUT1=0
+VOUT2=1
+SEQUENCE=4294967293  # from the example in docs: "fdffffff" for RBF
+RECIPIENT="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
+AMOUNT=0.20000000
 
-# Decode the PSBT and extract the receiving address from vout[0]
-ADDRESS=$(bitcoin-cli -regtest -named decodepsbt psbt=$PSBT | jq -r '.tx.vout[0].scriptPubKey.addresses[0]')
+# Create raw transaction following docs example exactly
+rawtx=$(bitcoin-cli -regtest -named createrawtransaction \
+  inputs='''[ 
+    { "txid": "'$TXID'", "vout": '$VOUT1', "sequence": '$SEQUENCE' },
+    { "txid": "'$TXID'", "vout": '$VOUT2', "sequence": '$SEQUENCE' }
+  ]''' \
+  outputs='''{ "'$RECIPIENT'": '$AMOUNT' }''')
 
-# Output the address
-echo $ADDRESS
+# Convert to PSBT exactly as shown in docs
+psbt=$(bitcoin-cli -regtest -named converttopsbt hexstring=$rawtx)
+
+# Output only the PSBT
+echo $psbt
